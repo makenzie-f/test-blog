f9b29c8b7470024a6746f1d8157c5e3917420948
makenzie-f <makenziefranco3@gmail.com>
2022-08-30 13:54:05 -0400

    Add css styling

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 </main>
 
-
+<footer>
+  <p>Page created by Makenzie Franco | August 2022</p>
+</footer>
 
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Intro Web Development</title>
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
@@ -13,4 +14,28 @@
     <h1>My Blog</h1>
   </header>
 
-  <main>
+  <nav class="flex-box">
+    <ul class="flex-item">
+      <!-- Need to use /~francom1/ because of Elvis directories -->
+      <li>
+        <a href="/~francom1/MA16/">Home</a>
+      </li>
+      <?php if (Auth::isLoggedIn()): ?>
+        <li>
+          <!-- Need to use /~francom1/ because of Elvis directories -->
+          <a href="/~francom1/MA16/admin/">Admin</a>
+        </li>
+        <li>
+          <!-- Need to use /~francom1/ because of Elvis directories -->
+          <a href="/~francom1/MA16/logout.php">Log out</a>
+        </li>
+      <?php else: ?>
+        <li>
+          <!-- Need to use /~francom1/ because of Elvis directories -->
+          <a href="/~francom1/MA16/login.php">Log in</a>
+        </li>
+      <?php endif; ?>
+    </ul>
+  </nav>
+
+  <main class="flex-box">
