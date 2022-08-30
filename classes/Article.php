28449a52ab793820f9df43f2a38dd1a5fcdbeec3
makenzie-f <makenziefranco3@gmail.com>
2022-08-30 13:51:43 -0400

    Add more classes

--- classes/Article.php
+++ classes/Article.php
@@ -27,6 +27,12 @@ class Article {
     */
   public $published_at;
 
+  /**
+    * Validation errors
+    * @var array
+    */
+  public $errors = [];
+
 /**
   * Get all articles
   *
@@ -71,4 +77,138 @@ class Article {
           return $stmt->fetch();
         }
   }
+/**
+  * Update the article with its current property values
+  *
+  * @param object $conn Connection to the database
+  *
+  * @return boolean True if the update was successful, false otherwise
+  *
+  */
+  public function update($conn){
+
+    if($this->validate()) {
+
+    $sql = "UPDATE article
+            SET title = :title,
+                content = :content,
+                published_at = :published_at
+            WHERE id = :id";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
+    $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+    $stmt->bindValue(':content', $this->content, PDO::PARAM_STR);
+
+      if ($this->published_at == '') {
+
+      $stmt->bindValue(':published_at', null, PDO::PARAM_NULL);
+      } else {
+
+      $stmt->bindValue(':published_at', $this->published_at, PDO::PARAM_STR);
+      }
+
+      return $stmt->execute();
+
+  } else {
+    return false;
+  }
+}
+
+  /**
+  * Validate the article properties
+  *
+  * @param string $title Title, required
+  * @param string $content Content, required
+  * @param string $published_at Published date and time, yyyy-mm-dd hh:mm:ss if not blank
+  *
+  * @return boolean True if the current properties are valid, false otherwise
+  */
+
+  protected function validate() {
+
+    if ($this->title == '') {
+      $this->errors[] = 'Title is required';
+    }
+
+    if ($this->content == '') {
+      $this->errors[] = 'Content is required';
+    }
+
+    if ($this->published_at != '') {
+      $date_time = date_create_from_format('Y-m-d\TH:i', $this->published_at);
+
+        if ($date_time === false){
+          $this->errors[] = 'Invalid date and time.';
+        } else {
+          $date_errors = date_get_last_errors();
+
+          if ($date_errors['warning_count'] > 0){
+            $this->errors[] = 'Invalid date and time.';
+          }
+        }
+    }
+
+    return empty($this->errors);
+  }
+/**
+  * Delete the current article
+  *
+  * @param object $conn Connection to the database
+  *
+  * @return boolean True if the delete was successful, false otherwise
+  *
+  */
+  public function delete($conn){
+
+    $sql = "DELETE FROM article
+            WHERE id = :id";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
+
+    return $stmt->execute();
+  }
+
+  /**
+    * Insert a new article with its current property values
+    *
+    * @param object $conn Connection to the database
+    *
+    * @return boolean True if the update was successful, false otherwise
+    *
+    */
+    public function create($conn){
+
+      if($this->validate()) {
+
+      $sql = "INSERT INTO article
+              (title, content, published_at)
+              VALUES (:title, :content, :published_at)";
+
+      $stmt = $conn->prepare($sql);
+
+      $stmt->bindValue(':title', $this->title, PDO::PARAM_STR);
+      $stmt->bindValue(':content', $this->content, PDO::PARAM_STR);
+
+        if ($this->published_at == '') {
+
+        $stmt->bindValue(':published_at', null, PDO::PARAM_NULL);
+        } else {
+
+        $stmt->bindValue(':published_at', $this->published_at, PDO::PARAM_STR);
+        }
+
+        if($stmt->execute()) {
+            $this->id = $conn->lastInsertID();
+            return true;
+        }
+
+    } else {
+      return false;
+    }
+  }
+
 }
