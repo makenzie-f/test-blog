fc68281320b1e2be722356f8b63d7327ef03d789
makenzie-f <makenziefranco3@gmail.com>
2022-08-30 13:34:07 -0400

    Create getDB function to connect to database

--- includes/database.php
+++ includes/database.php
@@ -13,3 +13,29 @@ if (mysqli_connect_error()){
 }
 
 // echo "Connected successfully!";
+<?php
+
+/**
+* Get the database connection
+*
+* @return object Connection to a MySQL server
+*/
+
+function getDB(){
+
+  $db_host = "localhost";
+  $db_name = "francom1";
+  $db_user = "francom1";
+  $db_pass = "1Badproblem!";
+
+  $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+  if (mysqli_connect_error()){
+      echo mysqli_connect_error();
+      exit;
+  }
+
+  return $conn;
+
+  // echo "Connected successfully!";
+}
